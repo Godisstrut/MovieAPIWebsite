27f362a4c7f57e578a179ffd6af829df2de03faf
Godisstrut <erikh13255@gmail.com>
2025-05-12 18:33:19 +0200

    Styling
    Added some styling for the search bar

--- my-movie-app/src/components/SearchBar.js
+++ my-movie-app/src/components/SearchBar.js
@@ -1,7 +1,7 @@
 function SearchBar() {
     return(
         <div className="search-bar">
-            <input type="text" placeholder="Search"></input>
+            <input type="text" placeholder="Search for a movie"></input>
         </div>
     )
 }
